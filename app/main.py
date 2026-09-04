5c9222017974a520c4844a9655177eee4d822d96
Neo Wang <932800614@qq.com>
2026-09-04 14:25:38 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -63,7 +63,7 @@ def main():
         print(message.content)
 
     # TODO: Uncomment the following line to pass the first stage
-    print(chat.choices[0].message.content)
+    # print(chat.choices[0].message.content)
 
 
 if __name__ == "__main__":
